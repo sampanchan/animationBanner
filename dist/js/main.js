cf74b5ca09a39562e463d385f4600fbfb6da3cf2
sampanchan <spankratz@my.creativecircus.edu>
2021-05-18 11:13:53 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+
+console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files."); //logos/info
+
+gsap.to(".logo", {
+  y: 300,
+  duration: 5,
+  delay: 10
+});
+gsap.from(".logo-border", {
+  opacity: 0,
+  duration: 10,
+  delay: 10
+});
+gsap.to(".switch-logo", {
+  rotation: 360,
+  opacity: 100,
+  duration: 5,
+  delay: 10
+});
+gsap.from(".e3", {
+  opacity: 0,
+  y: -100,
+  duration: 5,
+  delay: 10
+}); //characters
+
+gsap.to(".switch", {
+  y: 900,
+  duration: 15
+});
+gsap.to(".pika", {
+  y: 100,
+  duration: 1,
+  delay: 14,
+  repeat: 4,
+  yoyo: true
+}); // gsap.to(".animal-crossing", {y:-80, duration:1, delay:4, repeat:4, yoyo:true})
+
+gsap.to(".mario", {
+  y: -80,
+  duration: 1,
+  delay: 14,
+  repeat: 4,
+  yoyo: true
+}); //delay
+// let switchCharacters = document.querySelectorAll("#characters")
+// gsap.delay(switchCharacters, 5)
+//# sourceMappingURL=main.js.map
